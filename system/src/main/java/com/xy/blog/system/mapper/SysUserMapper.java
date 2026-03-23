20978cb0cc6d306af0c1cee1f1d8543992737616
letzq <489572770@qq.com>
2026-03-24 02:09:14 +0800

    完成注册登录接口

--- system/src/main/java/com/xy/blog/system/mapper/SysUserMapper.java
+++
@@ -1,7 +0,0 @@
-package com.xy.blog.system.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.xy.blog.system.domain.entity.SysUser;
-
-public interface SysUserMapper extends BaseMapper<SysUser> {
-}
--- system/src/main/java/com/xy/blog/system/service/ISysUserService.java
+++
@@ -1,9 +0,0 @@
-package com.xy.blog.system.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.xy.blog.system.domain.entity.SysUser;
-
-public interface ISysUserService extends IService<SysUser> {
-
-    SysUser getByUserName(String userName);
-}
